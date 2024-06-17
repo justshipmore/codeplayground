0c358d792642962d97044761e7dfa1d378166138
justshipmore <justshipmore@gmail.com>
2024-06-17 22:53:45 +0100

    Use usePanel hook in console panel

--- components/codepanel/select-language.tsx
+++ components/codepanel/select-language.tsx
@@ -10,7 +10,7 @@ export default function SelectLanguage() {
     <div className="flex gap-2 px-6 py-4 items-center">
       {LANGUAGE_PANELS.map((panel) => (
         <button
-          key={`${SelectLanguage}--${panel.slug}`}
+          key={`SelectLanguage--${panel.slug}`}
           data-slug={panel.slug}
           className={cn(
             `px-3 py-2 bg-neutral-800 text-neutral-300 rounded-full text-xs`,
