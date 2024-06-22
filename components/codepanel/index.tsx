07bed7597a25421811f03fd9e4f4a10c5a5cee8e
justshipmore <justshipmore@gmail.com>
2024-06-22 17:50:35 +0100

    Remove grid classes from preview and code panels

--- components/codepanel/index.tsx
+++ components/codepanel/index.tsx
@@ -2,12 +2,12 @@ import SelectLanguage from "./select-language";
 
 export default function CodePanel() {
   return (
-    <section className="bg-neutral-900 h-full flex flex-col flex-grow rounded-lg">
+    <div className="bg-neutral-900 flex flex-col w-1/2 rounded-lg">
       <SelectLanguage />
 
       <div className="px-6 py-4 text-neutral-300 text-sm">
         <p>Content goes here </p>
       </div>
-    </section>
+    </div>
   );
 }
--- components/previewpanel/index.tsx
+++ components/previewpanel/index.tsx
@@ -2,10 +2,10 @@ import OpenInTab from "./open-in-tab";
 
 export default function PreviewPanel() {
   return (
-    <section className="bg-neutral-50 h-full flex flex-col flex-grow rounded-lg gap-2">
+    <div className="bg-neutral-50 flex flex-col w-1/2 rounded-lg">
       <div className="px-6 py-4 text-neutral-900 text-sm">
         <p>Hello World</p>
       </div>
-    </section>
+    </div>
   );
 }
