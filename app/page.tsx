de646b3b5749d48540792238e9f0f4df6b519ed0
justshipmore <justshipmore@gmail.com>
2024-06-22 18:04:51 +0100

    Add styles to expand and collapse console panel

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 import CodePanel from "@/components/codepanel";
+import ConsolePanel from "@/components/consolepanel";
 import PreviewPanel from "@/components/previewpanel";
 
 export default function Home() {
   return (
     <main className="px-2 pt-2 h-screen text-white">
-      <div className="flex flex-col gap-2 h-full">
+      <div className="flex flex-col h-full">
         <section className="flex grow gap-2">
           <CodePanel />
           <PreviewPanel />
         </section>
 
-        {/* Console panel */}
-        <section className="bg-blue-300 ">
-          <p>Console panels</p>
-        </section>
+        <ConsolePanel />
       </div>
     </main>
   );
--- components/consolepanel/index.tsx
+++ components/consolepanel/index.tsx
@@ -8,33 +8,35 @@ export default function ConsolePanel() {
   const PANEL = LAYOUT_PANELS.filter((panel) => panel.slug === "console")[0];
 
   return (
-    <section className="flex flex-col py-4 px-6 bg-neutral-950 col-span-2 justify-end">
+    <section
+      className={`flex flex-col px-3 py-3 ${
+        isPanelOpen(PANEL) ? "h-2/6" : undefined
+      }`}
+    >
       {isPanelOpen(PANEL) && (
         <div className="text-neutral-300 text-sm flex-grow">
           Content goes here
         </div>
       )}
-      <div>
-        <button
-          key={`ConsolePanel-button--${PANEL.slug}`}
-          className="flex text-neutral-300 gap-1 items-center text-xs"
-          onClick={() => togglePanel(PANEL)}
+      <button
+        key={`ConsolePanel-button--${PANEL.slug}`}
+        className="flex text-neutral-300 gap-1 items-center text-xs"
+        onClick={() => togglePanel(PANEL)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-4"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="size-4"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm3.97.97a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 0 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Zm4.28 4.28a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          {PANEL.name}
-        </button>
-      </div>
+          <path
+            fillRule="evenodd"
+            d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm3.97.97a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 0 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Zm4.28 4.28a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        {PANEL.name}
+      </button>
     </section>
   );
 }
