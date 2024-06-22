b6f4e4233120c11570f64a53ac43967d5d3e5191
justshipmore <justshipmore@gmail.com>
2024-06-22 17:45:18 +0100

    Fix home page layout issue by using flexbox instead of grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import CodePanel from "@/components/codepanel";
-import ConsolePanel from "@/components/consolepanel";
 import PreviewPanel from "@/components/previewpanel";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 gap-2 px-2 pt-2 h-screen">
-      <CodePanel />
-      <PreviewPanel />
-      <ConsolePanel />
+    <main className="px-2 pt-2 h-screen text-white">
+      <div className="flex flex-col gap-2 h-full">
+        <section className="flex grow gap-2">
+          <CodePanel />
+          <PreviewPanel />
+        </section>
+
+        {/* Console panel */}
+        <section className="bg-blue-300 ">
+          <p>Console panels</p>
+        </section>
+      </div>
     </main>
   );
 }
