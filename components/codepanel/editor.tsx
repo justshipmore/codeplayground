7e8a7397e537dd260890a77bf2aec92437817dd9
justshipmore <justshipmore@gmail.com>
2024-06-29 20:59:16 +0100

    Add fix to remove duplicate editor and hydration warning from next

--- components/codepanel/editor.tsx
+++ components/codepanel/editor.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
+import { useEffect, useRef, useState } from "react";
+import { Controlled as ControlledEditor } from "react-codemirror2";
 
 // need to avoid throwing a ReferenceError of navigator is not defined
 let languageLoaded = false;
@@ -13,10 +17,58 @@ if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
 export default function Editor({
   isVisible,
   language,
+  value,
+  onChange,
 }: {
   isVisible: boolean;
   language: string;
+  value: string;
+  onChange: any;
 }) {
-  if (!isVisible) return;
-  return <p>Content goes here for {language} </p>;
+  const [isMounted, setMount] = useState(false); // Fix for hydration error from Next
+  const editorRef = useRef();
+  const wrapperRef = useRef();
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  const onBeforeChange = (
+    editor: CodeMirror.Editor,
+    data: CodeMirror.EditorChange,
+    value: string
+  ) => onChange(value);
+
+  if (!isVisible || !isMounted) return;
+  return (
+    <div className="bg-red-600 h-full">
+      <ControlledEditor
+        className="CodeMirrorWrapper"
+        onBeforeChange={onBeforeChange}
+        value={value}
+        options={{
+          lineWrapping: true,
+          mode: language,
+          theme: "material",
+          lineNumbers: true,
+        }}
+        // fix to remove duplicate editor bug in react-codemirror2: https://github.com/scniro/react-codemirror2/issues/284
+        editorDidMount={(editorElement) => {
+          (
+            editorRef as unknown as React.MutableRefObject<CodeMirror.Editor>
+          ).current = editorElement;
+        }}
+        // fix to remove duplicate editor bug in react-codemirror2: https://github.com/scniro/react-codemirror2/issues/284
+        editorWillUnmount={() => {
+          const editorWrapper = (
+            editorRef as unknown as React.MutableRefObject<CodeMirror.Editor>
+          ).current.getWrapperElement();
+          if (editorWrapper) editorWrapper.remove();
+          if (wrapperRef.current) {
+            (wrapperRef.current as { hydrated: boolean }).hydrated = false;
+          }
+        }}
+      />
+    </div>
+  );
 }
--- components/codepanel/index.tsx
+++ components/codepanel/index.tsx
@@ -1,12 +1,28 @@
 "use client";
 
-import { LANGUAGE_PANELS } from "@/utils/constants/panels";
+import { LANGUAGE_PANELS, PANEL_SLUG } from "@/utils/constants/panels";
 import SelectLanguage from "./select-language";
 import Editor from "./editor";
 import usePanel from "@/utils/hooks/use-panel";
+import { useState } from "react";
 
 export default function CodePanel() {
   const { isPanelOpen } = usePanel();
+  const [xml, setXML] = useState("");
+  const [css, setCSS] = useState("");
+  const [js, setJS] = useState("");
+
+  const getValue = (panel: PANEL_SLUG) => {
+    switch (panel) {
+      case "js" as string:
+        return js;
+      case "css":
+        return css as string;
+        break;
+      default:
+        return xml as string;
+    }
+  };
 
   return (
     <div className="bg-neutral-900 flex flex-col w-1/2 rounded-lg">
@@ -16,6 +32,18 @@ export default function CodePanel() {
           key={`Editor--${panel.language}`}
           isVisible={isPanelOpen(panel)}
           language={panel.language as string}
+          value={getValue(panel.slug)}
+          onChange={(value: string) => {
+            switch (panel.slug) {
+              case "js":
+                return setJS(value);
+              case "css":
+                return setCSS(value);
+                break;
+              default:
+                return setXML(value);
+            }
+          }}
         />
       ))}
     </div>
